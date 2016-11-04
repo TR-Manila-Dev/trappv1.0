b7f12fa465c222043cc3fb0a73f4b69be378f5c8
nullLogic2408 <mar.angelo.logic@gmail.com>
2016-11-04 21:57:09 +0800

    Added class beans and service controllers, updated pom.xml

--- src/main/java/com/trapp/services/trapp/MyResource.java
+++ src/main/java/com/trapp/services/trapp/MyResource.java
@@ -1,10 +1,15 @@
 package com.trapp.services.trapp;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.codehaus.jettison.json.*;
+import javax.json.*;
+import com.util.beans.TrackBeans;
 /**
  * Root resource (exposed at "myresource" path)
  */
@@ -22,4 +27,55 @@ public class MyResource {
     public String getIt() {
         return "Got it!";
     }
+    
+    @Path("/savenewasesurvey")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response CreateNewSurveyASE(String DATA) {
+		boolean success = false;
+		try {
+			Response rb = null;
+			JSONObject json = new JSONObject(DATA);
+			String surveyName = json.optString("surveyName", "");
+			String surveyDesc = json.optString("surveyDesc", "");
+			String surveyCreator = json.optString("surveyCreator", "");
+			String isAnonymous = json.optString("isAnonymous", "");
+
+			String surveyType = json.optString("surveyType", "");
+			String quarter = json.optString("quarter", "");
+			String year = json.optString("year", "");
+
+			TrackBeans trb = new TrackBeans();
+
+			JSONObject JSONreturn = trb.InsertQuestionsAndAnswerTypeAse("");
+			
+			String surveyId = JSONreturn.optString("surveyId", "");
+			StringBuilder sb = new StringBuilder();
+
+			if (JSONreturn.optString("error", "").equals("")) {
+				sb.append("{\"success\":\"success\",\"surveyId\":\"");
+				sb.append(surveyId + "\"}");
+				rb = Response.ok(sb.toString()).build();
+			} else {
+				sb.append("{\"error\":\"" + JSONreturn.optString("error") + "\"}");
+				rb = Response.status(500).entity("{\"error\":\"Name already exists\"}").build();
+			}
+
+			return rb;
+
+		} catch (NullPointerException h) {
+
+			return Response.status(400).entity("You have empty parameter").build();
+
+		} catch (Exception e) {
+			Response.ok("{\"error\":\"" + e.getMessage() + "\"}").build();
+		} finally {
+			success = false;
+		}
+
+		return Response.status(500)
+				.entity("{\"error\":\"There was an error on the code kindly contact marangelo.delatorre@thomsonreuters.com\"}")
+				.build();
+
+	}
 }
